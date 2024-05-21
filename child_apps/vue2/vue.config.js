12c3d27452f68cb9dba2f92298ffaf14eb7c2ed8
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2024-05-21 18:40:20 +0800

    feat: 🎸 Init Project

---
+++ child_apps/vue2/vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: 'vue2',
+  publicPath: '/child/vue2/',
+  productionSourceMap: false,
+  devServer: {
+    hot: false,
+    disableHostCheck: true,
+    port: 4008,
+    open: false,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  lintOnSave: false,
+  // 自定义webpack配置
+  configureWebpack: {
+
+  },
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+// 初始化micro-app
+import microApp from '@micro-zoe/micro-app';
+
+microApp.start()
